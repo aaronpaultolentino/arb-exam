8971b0ec6d311b6271fdd92ae69ad783ecef0ffd
Aaron Tolentino <aarontolentino123@gmail.com>
2021-04-23 00:32:44 +0800

    remove docker files

--- public/js/app.js
+++ public/js/app.js
@@ -51016,8 +51016,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/aaron/Documents/study/ringcentral/rc-server-monitoring-frontend/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/aaron/Documents/study/ringcentral/rc-server-monitoring-frontend/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/aaron/Documents/study/ringcentral/rc-pr/dev-exam-frontend/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/aaron/Documents/study/ringcentral/rc-pr/dev-exam-frontend/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
